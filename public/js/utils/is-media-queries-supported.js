8736d2a53aa8b70af36331307ea67ff4b2ca647f
Alex Binetskyi <ab@softevol.com>
2023-02-23 15:55:00 +0200

    Fix public branch.

--- public/js/utils/is-media-queries-supported.js
+++
@@ -1,4 +0,0 @@
-// Checking if the browser supports media queries.
-window.isMediaQueriesSupported = function() {
-  return (typeof window.matchMedia != 'undefined' || typeof window.msMatchMedia != 'undefined' ||  typeof window.MediaError != 'undefined');
-};
